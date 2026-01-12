78e0d9495bfcd2a20a80c6786d4a41630d563fe8
Jeandre <13597222+jeandrek@users.noreply.github.com>
2026-01-12 20:45:39 +1300

    Misc changes to main-x11.c

--- main-x11.c
+++ main-x11.c
@@ -12,6 +12,7 @@
 int
 respond_to(Display *dpy, XEvent *evt, XPointer arg)
 {
+	return 1;
 }
 
 int state[NUM_BUTTONS] = {0};
@@ -19,41 +20,50 @@ int state[NUM_BUTTONS] = {0};
 int
 main()
 {
-	Display *dpy = XOpenDisplay(":0");
-	int numFbConfs;
-
-	XSetWindowAttributes winAttrib;
-	winAttrib.event_mask = SubstructureNotifyMask|KeyPressMask|KeyReleaseMask;
-
-	Window win = XCreateWindow(dpy, DefaultRootWindow(dpy), 30, 30,
-				   800, 600, 0,
-				   CopyFromParent, InputOutput, CopyFromParent,
-				   CWEventMask, &winAttrib);
-
-	char *name = "Checkers";
-	XTextProperty prop;
-	XStringListToTextProperty(&name, 1, &prop);
-	XSetWMName(dpy, win, &prop);
-	Atom protocols[1];
-	protocols[0] = XInternAtom(dpy, "WM_DELETE_WINDOW", 1);
-	XSetWMProtocols(dpy, win, protocols, 1);
-
-	XEvent evt;
-
-	int attribs[] = {
+	int glx_attribs[] = {
 		GLX_RED_SIZE, 8, GLX_GREEN_SIZE, 8, GLX_BLUE_SIZE, 8,
 		GLX_DEPTH_SIZE, 16,
 		GLX_DOUBLEBUFFER, 1,
 		None
 	};
+	XSetWindowAttributes win_attribs;
+	int8_t keycode_buttons[256];
+	char *name = "Checkers";
+	XTextProperty prop;
+	Atom protocols[1];
+	GLXFBConfig *fb_confs;
+	int num_fb_confs;
+	GLXWindow glxWin;
+	GLXContext ctx;
+	Display *dpy;
+	Window win;
+	XEvent evt;
 
-	GLXFBConfig *fbconf = glXChooseFBConfig(dpy, 0, attribs, &numFbConfs);
-	if (numFbConfs == 0) return 1;
-	GLXWindow glxWin = glXCreateWindow(dpy, fbconf[0], win, NULL);
-	GLXContext ctx = glXCreateNewContext(dpy, fbconf[0],
-					     GLX_RGBA_TYPE, NULL, 1);
+	dpy = XOpenDisplay(":0");
+
+	win_attribs.event_mask = (SubstructureNotifyMask
+				  | KeyPressMask
+				  | KeyReleaseMask);
+
+	win = XCreateWindow(dpy, DefaultRootWindow(dpy), 30, 30,
+			    800, 600, 0,
+			    CopyFromParent, InputOutput, CopyFromParent,
+			    CWEventMask, &win_attribs);
+
+	XStringListToTextProperty(&name, 1, &prop);
+	XSetWMName(dpy, win, &prop);
+	protocols[0] = XInternAtom(dpy, "WM_DELETE_WINDOW", 1);
+	XSetWMProtocols(dpy, win, protocols, 1);
+
+	fb_confs = glXChooseFBConfig(dpy, 0, glx_attribs, &num_fb_confs);
+	if (num_fb_confs == 0)
+		return 1;
+
+	glxWin = glXCreateWindow(dpy, fb_confs[0], win, NULL);
+	ctx = glXCreateNewContext(dpy, fb_confs[0],
+				  GLX_RGBA_TYPE, NULL, 1);
 	glXMakeCurrent(dpy, glxWin, ctx);
-	XFree(fbconf);
+	XFree(fb_confs);
 
 	XMapWindow(dpy, win);
 
@@ -65,7 +75,6 @@ main()
 
 	XkbSetDetectableAutoRepeat(dpy, True, NULL);
 
-	int8_t keycode_buttons[256];
 	memset(keycode_buttons, -1, 256);
 	for (int i = 0; i < 256; i++) {
 		KeySym ks = XkbKeycodeToKeysym(dpy, i, 0, 0);
@@ -86,11 +95,11 @@ main()
 		switch (evt.type) {
 		case KeyPress:
 			if (keycode_buttons[evt.xkey.keycode] >= 0)
-			state[keycode_buttons[evt.xkey.keycode]] = 1;
+				state[keycode_buttons[evt.xkey.keycode]] = 1;
 			break;
 		case KeyRelease:
 			if (keycode_buttons[evt.xkey.keycode] >= 0)
-			state[keycode_buttons[evt.xkey.keycode]] = 0;
+				state[keycode_buttons[evt.xkey.keycode]] = 0;
 			break;
 		case ClientMessage:
 			goto quit;
@@ -98,6 +107,6 @@ main()
 	}
  quit:
 	XDestroyWindow(dpy, win);
-
 	XCloseDisplay(dpy);
+	return 0;
 }
