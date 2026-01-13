d8a01528ac2f9745841f1b130328e239825ec063
Jeandre <13597222+jeandrek@users.noreply.github.com>
2026-01-13 16:50:54 +1300

    Reindent mac.c/minor changes

--- mac.c
+++ mac.c
@@ -30,77 +30,83 @@ static OSStatus WindowEventHandler(EventHandlerCallRef inCaller, EventRef inEven
 int
 main(void)
 {
-  OSStatus err;
-  WindowRef window;
-  static EventTypeSpec kWindowEvents[] = {
-    {kEventClassWindow, kEventWindowDrawContent},
-    {kEventClassWindow, kEventWindowBoundsChanged},
-    {kEventClassWindow, kEventWindowActivated}
-  };
-  CGrafPtr port;
-  GLint attribs[] = {AGL_RGBA, AGL_DOUBLEBUFFER, AGL_DEPTH_SIZE, 24, AGL_NONE};
-  AGLPixelFormat pix;
-  GLint swap_interval[] = {1};
-  Rect bounds = {100, 100, 100+HEIGHT, 100+WIDTH};
+	OSStatus err;
+	WindowRef window;
+	static EventTypeSpec kWindowEvents[] = {
+		{kEventClassWindow, kEventWindowDrawContent},
+		{kEventClassWindow, kEventWindowBoundsChanged},
+		{kEventClassWindow, kEventWindowActivated}
+	};
+	CGrafPtr port;
+	GLint attribs[] = {
+		AGL_RGBA,
+		AGL_DOUBLEBUFFER,
+		AGL_DEPTH_SIZE, 24,
+		AGL_NONE
+	};
+	AGLPixelFormat pix;
+	GLint swap_interval[] = {1};
+	Rect bounds = {100, 100, 100+HEIGHT, 100+WIDTH};
 
-  err = CreateNewWindow(kDocumentWindowClass,
-			(kWindowStandardFloatingAttributes |
-			 kWindowStandardHandlerAttribute),
-			&bounds, &window);
+	err = CreateNewWindow(kDocumentWindowClass,
+			      (kWindowStandardFloatingAttributes |
+			       kWindowStandardHandlerAttribute),
+			      &bounds, &window);
 
-  InstallEventHandler(GetWindowEventTarget(window),
-		      NewEventHandlerUPP(WindowEventHandler),
-		      sizeof (kWindowEvents) / sizeof (EventTypeSpec),
-		      kWindowEvents,
-		      window, NULL);
+	InstallEventHandler(GetWindowEventTarget(window),
+			    NewEventHandlerUPP(WindowEventHandler),
+			    sizeof (kWindowEvents) / sizeof (EventTypeSpec),
+			    kWindowEvents,
+			    window, NULL);
 
-  pix = aglChoosePixelFormat(NULL, 0, attribs);
-  context = aglCreateContext(pix, NULL);
-  aglDestroyPixelFormat(pix);
-  port = GetWindowPort(window);
-  aglSetDrawable(context, port);
-  aglSetCurrentContext(context);
-  aglSetInteger(context, AGL_SWAP_INTERVAL, swap_interval);
+	pix = aglChoosePixelFormat(NULL, 0, attribs);
+	context = aglCreateContext(pix, NULL);
+	aglDestroyPixelFormat(pix);
+	port = GetWindowPort(window);
+	aglSetDrawable(context, port);
+	aglSetCurrentContext(context);
+	aglSetInteger(context, AGL_SWAP_INTERVAL, swap_interval);
 
-  sg_init(WIDTH, HEIGHT);
-  checkers_init();
+	sg_init(WIDTH, HEIGHT);
+	checkers_init();
 
-  ShowWindow(window);
+	ShowWindow(window);
 
-  RunApplicationEventLoop();
+	RunApplicationEventLoop();
 
-  return err;
+	return err;
 }
 
 static OSStatus
-WindowEventHandler(EventHandlerCallRef inCaller, EventRef inEvent, void* inRefcon)
+WindowEventHandler(EventHandlerCallRef inCaller, EventRef inEvent,
+		   void *inRefcon)
 {
-  OSStatus err = eventNotHandledErr;
+	OSStatus err = eventNotHandledErr;
+	WindowRef window;
 
-  switch (GetEventClass(inEvent)) {
-  case kEventClassWindow:
-    {
-      switch (GetEventKind(inEvent)) {
-      case kEventWindowActivated:
-      case kEventWindowBoundsChanged:
-	aglUpdateContext(context);
-	break;
-      case kEventWindowDrawContent:
-	{
-	  WindowRef window;
-	  Rect rect = {0, 0, HEIGHT, WIDTH};
-	  GetEventParameter(inEvent, '----', 'wind', NULL, sizeof (WindowRef), NULL, &window);
-	  input_handle();
-	  checkers_update();
-	  aglSwapBuffers(context);
-	  InvalWindowRect(window, &rect);
+	GetEventParameter(inEvent, '----', 'wind', NULL,
+			  sizeof (WindowRef), NULL, &window);
+
+	switch (GetEventClass(inEvent)) {
+	case kEventClassWindow:
+		switch (GetEventKind(inEvent)) {
+		case kEventWindowActivated:
+		case kEventWindowBoundsChanged:
+			aglUpdateContext(context);
+			break;
+		case kEventWindowDrawContent:
+			{
+				Rect rect = {0, 0, HEIGHT, WIDTH};
+				input_handle();
+				checkers_update();
+				aglSwapBuffers(context);
+				InvalWindowRect(window, &rect);
+			}
+			break;
+		}
+	default:
+		break;
 	}
-	break;
-      }
-    }
-  default:
-    break;
-  }
-  return err;
+	return err;
 }
 
