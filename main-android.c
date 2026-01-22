161a98f78032c5a2983c3d8a7bfa662407ec9e41
Jeandre <13597222+jeandrek@users.noreply.github.com>
2026-01-22 19:57:37 +1300

    Use a big lock (allows input)

--- main-android.c
+++ main-android.c
@@ -5,6 +5,7 @@
  */
 
 #include <jni.h>
+#include <threads.h>
 #include <GLES/gl.h>
 
 #include "checkers.h"
@@ -15,26 +16,37 @@ JNIEnv *checkers_jnienv;
 /* jeandre.checkers.Checkers class */
 jclass checkers_java;
 
+static mtx_t checkers_mutex;	/* Big lock */
+
 JNIEXPORT void JNICALL
 Java_jeandre_checkers_Checkers_init(JNIEnv *env, jobject obj,
 				    jint width, jint height)
 {
 	checkers_jnienv = env;
 	checkers_java = obj;
+
+	mtx_init(&checkers_mutex, mtx_plain);
+
 	sg_init(width, height);
 	checkers_init();
-	//input_init();
 }
 
 JNIEXPORT void JNICALL
 Java_jeandre_checkers_Checkers_update(JNIEnv *env, jobject obj)
 {
-	//input_handle();
+	/* input_handle(); */
+	mtx_lock(&checkers_mutex);
 	checkers_update();
+	mtx_unlock(&checkers_mutex);
 }
 
 JNIEXPORT void JNICALL
 Java_jeandre_checkers_Checkers_inputEvent(JNIEnv *env, jobject obj, jint button)
 {
-	checkers_input_event(button); /* XXX */
+	mtx_lock(&checkers_mutex);
+	/* Alternatively, this could enqueue events and input_handle processes
+	   them.  This is fine for now but it probably needs to be done that
+	   way with networking/computer opponent. */
+	checkers_input_event(button);
+	mtx_unlock(&checkers_mutex);
 }
